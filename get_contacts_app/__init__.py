b13a63306fa0c51f6aa4bedb423478d14da853ab
nathanjh-28 <nathan@nathanjharris.com>
2021-01-06 19:00:21 -0700

    heroku deployment 1

--- get_contacts_app/__init__.py
+++ get_contacts_app/__init__.py
@@ -7,8 +7,11 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '2APngFaCxp6CZ1jG'
 
+#Dev 2 of 2
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#Prod
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lbfwidkcmtpqht:f1ee0c93db2ed278ea614b7f2e2182b0c7d8da3a6f2282faace6ae49674ac5bc@ec2-50-19-247-157.compute-1.amazonaws.com:5432/d17vful3o0p4nc'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from get_contacts_app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # change for dev 1 of 2
+    app.run(debug=False)
