d9203e0a9f1ed28029049e7fd6cd032ce51d0a97
nathanjh-28 <nathan@nathanjharris.com>
2021-01-06 19:18:05 -0700

    deployment 3

--- get_contacts_app/__init__.py
+++ get_contacts_app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
@@ -10,7 +11,9 @@ app.config['SECRET_KEY'] = '2APngFaCxp6CZ1jG'
 #Dev 2 of 2
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 #Prod
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lbfwidkcmtpqht:f1ee0c93db2ed278ea614b7f2e2182b0c7d8da3a6f2282faace6ae49674ac5bc@ec2-50-19-247-157.compute-1.amazonaws.com:5432/d17vful3o0p4nc'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
