246e6bb652ac2d1766de4a04945aa61192e43885
nathanjh-28 <nathan@nathanjharris.com>
2021-01-06 19:11:09 -0700

    fixed error, ready for deployment 2

--- get_contacts_app/models.py
+++ get_contacts_app/models.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True,nullable=False)
-    image = db.Column(db.String(60), nullable=False,default=default_image)
+    image = db.Column(db.String(200), nullable=False,default=default_image)
     password = db.Column(db.String(60), nullable=False)
     bio = db.Column(db.String(280))
     posts = db.relationship('Post',backref='post_author', lazy=True)
